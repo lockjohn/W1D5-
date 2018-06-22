03bf89060b31b7665ea85170b775a22db8e03cf2
lockjohn <lockhartjohn527@gmail.com>
2018-06-22 11:04:54 -0700

    Add basic variables to PolyTreeNode class

---
+++ polytree_exercise/lib/00_tree_node.rb
@@ -0,0 +1,23 @@
+class PolyTreeNode
+  
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+  
+  def parent
+    parent  
+  end
+  
+  def children
+    children
+  end
+  
+  def value
+    value
+  end
+    
+  private
+  attr_reader :parent, :children, :value
+end
